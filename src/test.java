92255166acaff6510d86373152cf1179266212d5
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-04-13 10:39:18 +0200

    test

--- src/test.java
+++ src/test.java
@@ -3,5 +3,6 @@ public class test {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("this is great");
+    System.out.println("test3");
   }
 }
