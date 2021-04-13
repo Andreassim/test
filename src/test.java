0430f3128f0b0f71d16dd7b93c76a1f1fea7b09e
Mathias Reker <mathias@reker.dk>
2021-04-13 11:11:05 +0200

    New test

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ public class test {
 
   public static void main(String[] args) {
     System.out.println("Hello World");
+    System.out.println("This is a change");
     System.out.println("this is great");
     System.out.println("test3");
   }
