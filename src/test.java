0800f1178ca7d55ec156e9ae81f9705f0fc02597
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-04-14 12:04:26 +0200

    Added new println to test.java
    fun times

--- src/test.java
+++ src/test.java
@@ -1,11 +1,1 @@
-public class test {
-//TEST
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-    System.out.println("This is a change");
-    System.out.println("this is great");
-    System.out.println("test3");
-    System.out.println("test wooow");
-  }
-
-}
+public class test {//TEST  public static void main(String[] args) {    System.out.println("Hello World");    System.out.println("This is a change");    System.out.println("this is great");    System.out.println("test3");    System.out.println("test wooow");    System.out.println("test5");  }}
